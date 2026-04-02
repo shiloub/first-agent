c311c2886279db34e6ceb42c26a5c7e3857aba3e
= <=>
2026-04-02 11:31:21 +0200

    not working, but complete

--- main.py
+++ main.py
@@ -1,6 +1,64 @@
-def main():
-    print("Hello from first-agent!")
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
 
 
-if __name__ == "__main__":
-    main()
+## read .env file, and then load the variables into env
+load_dotenv()
+
+class ResearchResponse(BaseModel):
+    ## All the output i want from the llm call
+    topic: str
+    summary: str
+    source: list[str]
+    tool_uses: list[str]
+
+
+## Loading my llm
+# llm = ChatOpenAI(model = "gpt-4o-mini", api_key="openai_api_key")
+# llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+
+
+tools = [search_tool, wiki_tool, save_tool]
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=tools
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+query = input("What can i help you research? ")
+raw_response = agent_executor.invoke({"query": query})
+
+try:
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_response)
+# response = llm.invoke(input = "hi Gemini, what is your version, and how are you ?")
+# print(response)
